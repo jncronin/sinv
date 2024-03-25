43a561379761c74833913350482f83acf524881b
John Cronin <jncronin@tysos.org>
2024-03-25 09:41:16 +0000

    Basic SDL setup

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_Rect r;
+    r.x = 100;
+    r.y = 100;
+    r.w = 100;
+    r.h = 100;
+    SDL_RenderFillRect(renderer, &r);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
